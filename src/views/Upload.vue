8466f09b2080d563742243b1b615d471932a00be
pigeon_gugugu <892197240@qq.com>
2022-02-05 17:04:18 +0800

    feat: finish upload page beta version

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,10 +1,17 @@
 <template>
 <div>
    <el-card style="text-align:center">
-       <el-upload class="upload-frame" :action="uploadURL" :headers="headers"  drag multiply>
-          
-           
+       <el-upload class="upload-frame" :action="uploadURL" :headers="headers"  drag multiple>
+          <img src="../assets/monster.png" height="110">
+          <div>
+           <em style="color: rgba(140, 90, 221, 0.808);">drag your file here</em>
+           <br>
+           <span style="color: rgba(156, 17, 184, 0.712);">or</span>
+           <br>
+           <em style="color: rgba(18, 129, 180, 0.753);">click to upload file</em>
+        </div>
        </el-upload>
+       
    </el-card>
 </div>
 </template>
